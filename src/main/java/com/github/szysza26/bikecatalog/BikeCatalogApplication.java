5b73854c857e48509294076358a2066116651334
szysza26 <szy162@gmail.com>
2023-12-10 12:16:29 +0100

    init spring boot project

---
+++ src/main/java/com/github/szysza26/bikecatalog/BikeCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.github.szysza26.bikecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/szysza26/bikecatalog/BikeCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.szysza26.bikecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
