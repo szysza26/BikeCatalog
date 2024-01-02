00147cb80fa6fa92242d49dae86d49840ee04492
szysza26 <szy162@gmail.com>
2024-01-02 15:15:03 +0100

    add logout button

--- src/main/java/com/github/szysza26/bikecatalog/controller/admin/AdminPanelController.java
+++ src/main/java/com/github/szysza26/bikecatalog/controller/admin/AdminPanelController.java
@@ -1,13 +1,16 @@
 package com.github.szysza26.bikecatalog.controller.admin;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminPanelController {
 
     @GetMapping("/admin")
-    public String index() {
+    public String index(Model model, Authentication authentication) {
+        model.addAttribute("authentication", authentication);
         return "admin/index";
     }
 
